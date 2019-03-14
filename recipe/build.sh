9c9e33e527eb83570f791cca6b230f75e940ef28
Duncan Macleod <duncan.macleod@ligo.org>
2019-03-14 10:59:28 +0000

    added parallel builds for mpi implementations

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+set -ex
+
+if [[ "${mpi}" == "nompi" ]]; then
+    MPI_ARGS="--disable-mpi"
+else
+    MPI_ARGS="--enable-mpi MPICC=${PREFIX}/bin/mpicc MPICXX=${PREFIX}/bin/mpicxx MPIFC=${PREFIX}/bin/mpifc"
+fi
+
 ./configure \
 	--prefix=${PREFIX} \
 	--disable-gcc-flags \
 	--enable-cfitsio \
 	--enable-openmp \
-	--enable-mpi \
-	MPICC=${PREFIX}/bin/mpicc \
-	MPICXX=${PREFIX}/bin/mpicxx \
-	MPIFC=${PREFIX}/bin/mpifc
+	${MPI_ARGS}
 
 make -j ${CPU_COUNT}
 make -j ${CPU_COUNT} check
